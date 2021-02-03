4b5b753a6292e31368151f9247cf75bc72103d34
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2021-02-03 14:34:57 +0000

    feat: added minefield initialisation

---
+++ src/minefield.js
@@ -0,0 +1,9 @@
+class Minefield {
+  constructor() {
+    this.size = Object();
+    this.size.rows = 3;
+    this.size.columns = 3;
+  }
+}
+
+module.exports = Minefield;
---
+++ test/minefield.test.js
@@ -0,0 +1,14 @@
+const Minefield = require('../src/minefield.js');
+
+describe('Minefield creation', () => {
+  let minefield;
+
+  beforeEach(() => {
+    minefield = new Minefield();
+  });
+
+  test('should initialise a minefield that is 3x3', () => {
+    expect(minefield.size.columns).toBe(3);
+    expect(minefield.size.rows).toBe(3);
+  });
+});
